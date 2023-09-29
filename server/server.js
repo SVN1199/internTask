af61b4a3bd6cb09bbcdbf5aca3f392de486ec4a6
SVN1199 <vasanthnov1199@gmail.com>
2023-09-30 01:05:02 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 const dotenv = require('dotenv').config();
 const config = require('./config/db')
 const userRouter = require('./route/userRoutes')
 const profileRouter = require('./route/profileRoute')
-const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
