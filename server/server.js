682bf62e4419e0de7cf59e697c22f1f55f843bb2
SVN1199 <vasanthnov1199@gmail.com>
2023-09-30 01:49:45 +0530

    path

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 5000
 const dotenv = require('dotenv').config();
 const config = require('./config/db')
