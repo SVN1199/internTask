3910e4f2085bc1315b2b7e9e2f8d821372d0701a
SVN1199 <vasanthnov1199@gmail.com>
2023-09-30 01:56:47 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const port = process.env.PORT || 5000
 const dotenv = require('dotenv').config();
 const config = require('./config/db')
@@ -10,15 +9,20 @@ const profileRouter = require('./route/profileRoute')
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.use('/api/profile', profileRouter)
+app.use('/api/users', userRouter)
+
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.use('/api/profile', profileRouter)
-app.use('/api/users', userRouter)
 
 app.listen(port, ()=>{
     console.log(`Server Running on ${port}`)
