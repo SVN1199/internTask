173c5e78e4e8f48178b9cd7df1776e6ff34b344a
SVN1199 <vasanthnov1199@gmail.com>
2023-09-30 12:01:50 +0530

    change

--- server/controller/profileController.js
+++ server/controller/profileController.js
@@ -1,66 +1,80 @@
-const asyncHandler = require('express-async-handler')
-const Profile = require('../model/profileModel')
+const asyncHandler = require('express-async-handler');
+const Profile = require('../model/profileModel');
 
-const getProfile = asyncHandler(async(req,res)=>{
-    const profile = await Profile.find({ user: req.user.id });
-    const reversedProfile = profile.reverse()[0];
+const getProfile = asyncHandler(async (req, res) => {
+  try {
+    const profiles = await Profile.find({ user: req.user.id });
+    const reversedProfile = profiles.reverse()[0];
 
     if (!reversedProfile) {
-        res.status(400).json({ error: 'No Data Found' });
-        return;
+      res.status(404).json({ error: 'No Data Found' });
+      return;
     }
 
     res.status(200).json(reversedProfile);
-})
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-const postProfile = asyncHandler(async(req,res)=>{
+const postProfile = asyncHandler(async (req, res) => {
+  try {
     const { name, fname, dob, gender, mobile, address } = req.body;
 
     if (!name || !fname || !dob || !gender || !mobile || !address) {
-        res.status(400).json({ error: 'Please fill all the fields' });
-        return;
-    }
-    
-        const profile = await Profile.create({
-            name  : req.body.name,
-            fname : req.body.fname,
-            dob   : req.body.dob,
-            gender: req.body.gender,
-            mobile: req.body.mobile,
-            address: req.body.address,
-            user: req.user.id
-        });
-    
-        res.status(200).json(profile);
-    } 
-)
-
-const putProfile = asyncHandler(async(req,res)=>{
-    const profile = await Profile.findById(req.params.id)
-    
-    if(!profile){
-        res.status(400)
-        throw new Error (' No Data Found')
-    }
-    
-
-    if(!req.user){
-        res.status(401)
-        throw new Error('User Not Found')
+      res.status(400).json({ error: 'Please fill all the fields' });
+      return;
     }
 
-    if(profile.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('User Not Authorized')
+    const profile = await Profile.create({
+      name,
+      fname,
+      dob,
+      gender,
+      mobile,
+      address,
+      user: req.user.id,
+    });
+
+    res.status(201).json(profile);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const putProfile = asyncHandler(async (req, res) => {
+  try {
+    const profile = await Profile.findById(req.params.id);
+
+    if (!profile) {
+      res.status(404).json({ error: 'No Data Found' });
+      return;
     }
 
-    const updateProfile = await Profile.findByIdAndUpdate(req.params.id, req.body, {new : true})
-    res.status(200).json(updateProfile)
-    })
+    if (!req.user) {
+      res.status(401).json({ error: 'User Not Found' });
+      return;
+    }
 
+    if (profile.user.toString() !== req.user.id) {
+      res.status(401).json({ error: 'User Not Authorized' });
+      return;
+    }
+
+    const updateProfile = await Profile.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updateProfile);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = {
-    getProfile,
-    postProfile,
-    putProfile,
-}
+  getProfile,
+  postProfile,
+  putProfile,
+};
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,78 +1,85 @@
-const asyncHandler = require('express-async-handler')
-const jwt = require('jsonwebtoken')
-const User = require('../model/userModel')
-const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
 
-const registerUser = asyncHandler(async(req, res)=>{
-    const {name, email, password} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    if(!name || !email || !password){
-        res.status(400)
-        throw new Error('Please fill the fields')
+    if (!name || !email || !password) {
+      res.status(400).json({ error: 'Please fill the fields' });
+      return;
     }
 
-    const userExists = await User.findOne({email})
+    const userExists = await User.findOne({ email });
 
-    if(userExists){
-        res.status(400)
-        throw new Error('User Already Exists')
+    if (userExists) {
+      res.status(400).json({ error: 'User Already Exists' });
+      return;
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create(
-        {
-            name,
-            email,
-            password : hashedPassword
-        }
-    )
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     if (user) {
-    res.status(200).json({
+      res.status(200).json({
         _id: user.id,
         name: user.name,
         email: user.email,
         token: generateToken(user._id),
-    });
-} else {
-    res.status(400).json({ error: 'Invalid User Data' });
-}
+      });
+    } else {
+      res.status(400).json({ error: 'Invalid User Data' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-})
+const loginUser = asyncHandler(async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-const loginUser = asyncHandler(async(req, res)=>{
-    const {email, password} = req.body
+    const user = await User.findOne({ email });
 
-    const user = await User.findOne({email})
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ error: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-    if(user && await bcrypt.compare(password, user.password)){
-        res.json({
-            _id : user.id,
-            name : user.name,
-            email : user.email,
-            token : generateToken(user._id)
-        })
-    }else {
-        res.status(400)
-        throw new Error('Invalid credentials')
-      }
-})
-
-const getUser = asyncHandler(async(req, res)=>{
-    res.status(200).json(req.user)
-})
-
+const getUser = asyncHandler(async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
-  }
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
-    registerUser,
-    loginUser,
-    getUser
-}
+  registerUser,
+  loginUser,
+  getUser,
+};
